b68ff895fc3f7611e4a5bdaaf8eb68c86126f864
DAVID Jean-Marc Portable B13U200 <jmdavid@sdis13.fr>
2019-09-23 17:20:13 +0200

    Rajout info user

--- Tailleprofils.ps1
+++ Tailleprofils.ps1
@@ -7,8 +7,11 @@ Install-Module PSFolderSize -Force
 Get-Date | Out-File ..\OK.txt -append -width 120
 foreach ($serverName in (get-content ..\servers.txt)) {
     $serverName = $serverName.Substring(0, 7)
-    $serverName
-    $Sqlinsert = "select o.odDateScan from otObject o where o.otInternalNumber='" + $serverName + "'"
+    #$serverName
+    #$Sqlinsert = "select o.odDateScan from otObject o where o.otInternalNumber='" + $serverName + "'"
+    $Sqlinsert ="SELECT otObject.odDateScan, [otobject_1].[otModel]+'  \ '+[otobject_2].[otModel]+'  \ '+[otobject_3].[otModel] AS Nom
+    FROM (((otObject INNER JOIN otObjectRelation ON otObject.oidObject = otObjectRelation.oidObject) INNER JOIN otObject AS otObject_1 ON otObjectRelation.oidObjectRelation = otObject_1.oidObject) INNER JOIN (otObjectRelation AS otObjectRelation_1 INNER JOIN otObject AS otObject_2 ON otObjectRelation_1.oidObjectRelation = otObject_2.oidObject) ON otObject.oidObject = otObjectRelation_1.oidObject) INNER JOIN (otObjectRelation AS otObjectRelation_2 INNER JOIN otObject AS otObject_3 ON otObjectRelation_2.oidObjectRelation = otObject_3.oidObject) ON otObject.oidObject = otObjectRelation_2.oidObject
+    WHERE (((otObject.otInternalNumber)='" + $serverName + "') AND ((otObjectRelation.oidObjectRelationType)=201) AND ((otObjectRelation_1.oidObjectRelationType)=302) AND ((otObjectRelation_2.oidObjectRelationType)=304))"
     $SqlConnection = new-object System.Data.SqlClient.SqlConnection
     $SqlConnection.ConnectionString = $connectionString
     $SqlCommand = $SqlConnection.CreateCommand()
@@ -17,9 +20,9 @@ foreach ($serverName in (get-content ..\servers.txt)) {
     $dataset = new-object System.Data.Dataset
     $DataAdapter.Fill($dataset) | Out-Null
     $dateinventaire = $dataset.Tables[0].Rows[0].Item(0)
-    $messageinventaire = $serverName + " --> Dernier inventaire le: " + $dateinventaire.tostring("dd-MM-yyyy") + " Testé le : " + (Get-Date).tostring("dd-MM-yyyy")
+    $messageinventaire = $serverName + " \ " + $dataset.Tables[0].Rows[0].Item(1) + " --> Dernier inventaire le: " + $dateinventaire.tostring("dd-MM-yyyy") + " Testé le : " + (Get-Date).tostring("dd-MM-yyyy")
     if ($dateinventaire -lt (Get-Date).AddMonths(-4)) { $messageinventaire = $messageinventaire + ' Non utilisé depuis plus de 4 Mois!!!!' } else { $messageinventaire = $messageinventaire + " OK!" } 
-
+    $messageinventaire
     #"Dernier inventaire le: " + $dataset.Tables[0].Rows[0].Item(0) 
 
     if ((Test-Connection -computername $serverName -Count 1 -quie) -eq "Success") {
